028e5ff01a3f28e957212393af66192c3945466c
rune <labs4rune@gmail.com>
2026-08-06 03:05:30 -0400

    fix(pty): fix TerminateProcess result logic inversion
    TerminateProcess utilizes non-zero values for success; as such, Err(err) path would be utilized for res state 0, failure, and not the != 0 case

--- src-tauri/patches/portable-pty/src/win/mod.rs
+++ src-tauri/patches/portable-pty/src/win/mod.rs
@@ -43,7 +43,7 @@ impl WinChild {
         let proc = self.proc.lock().unwrap().try_clone().unwrap();
         let res = unsafe { TerminateProcess(proc.as_raw_handle() as _, 1) };
         let err = IoError::last_os_error();
-        if res != 0 {
+        if res == 0 {
             Err(err)
         } else {
             Ok(())
@@ -72,7 +72,7 @@ impl ChildKiller for WinChildKiller {
     fn kill(&mut self) -> IoResult<()> {
         let res = unsafe { TerminateProcess(self.proc.as_raw_handle() as _, 1) };
         let err = IoError::last_os_error();
-        if res != 0 {
+        if res == 0 {
             Err(err)
         } else {
             Ok(())
