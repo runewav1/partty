9cc2dee0325e9764434dbe7a68530c06290394af
rune <labs4rune@gmail.com>
2026-08-10 16:26:08 -0400

    fix: capitalize TERM_PROGRAM and bump all TERM_PROGRAM_VERSION values -> 0.3.0

--- src-tauri/scripts/partty-shell-integration.ps1
+++ src-tauri/scripts/partty-shell-integration.ps1
@@ -251,5 +251,5 @@ if ($initialCwd) {
 }
 
 $env:PARTTY_SHELL_INTEGRATION = "1"
-$env:TERM_PROGRAM = "partty"
-$env:TERM_PROGRAM_VERSION = "0.2.0"
+$env:TERM_PROGRAM = "Partty"
+$env:TERM_PROGRAM_VERSION = "0.3.0"
